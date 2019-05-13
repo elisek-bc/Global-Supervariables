d742def1aaedc1bafa908482dd25081a6678b973
elisek-bc <elisekppns@gmail.com>
2019-05-13 10:57:51 +0200

    convert to forms

--- src/setup.php
+++ src/setup.php
@@ -14,26 +14,72 @@
         <!--POST PART-->
         <div class="row poster">
             <div class="col-6 movies">
-                <h2>Top 5 Disney movies</h2>
-                <ol>
-                    <li>Up!</li>
-                    <li>The Lion King</li>
-                    <li>Wall-e</li>
-                    <li>Rapunzel</li>
-                    <li>Big Hero 6</li>
-                </ol>
+                <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+                    <h2>Top 5 Disney movies</h2>
+                    <input type="text" name="movie1" value="Up!">
+                    <br>
+                    <input type="text" name="movie2" value="The Lion King">
+                    <br>
+                    <input type="text" name="movie3" value="Wall-e">
+                    <br>
+                    <input type="text" name="movie4" value="Rapunzel">
+                    <br>
+                    <input type="text" name="movie5" value="Big Hero 6">
+                    <br>
+                    <input type="submit" value="Submit">
+                </form> 
             </div>
+
+            <?php
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                // collect value of input field
+            $name = htmlspecialchars($_REQUEST['movie1']);
+            echo $name;
+            $name2 = htmlspecialchars($_REQUEST['movie2']);
+            echo $name2;
+            $name3 = htmlspecialchars($_REQUEST['movie3']);
+            echo $name3;
+            $name4 = htmlspecialchars($_REQUEST['movie4']);
+            echo $name4;
+            $name5 = htmlspecialchars($_REQUEST['movie5']);
+            echo $name5;
+            }
+            ?>
+
             <div class="col-6 shows">
-            <h2>Top 5 shows</h2>
-                <ol>
-                    <li>Money Heist</li>
-                    <li>New girl</li>
-                    <li>You me her</li>
-                    <li>Atypical</li>
-                    <li>Riverdale</li>
-                </ol>
+                <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+                    <h2>Top 5 tv shows</h2>
+                    <input type="text" name="show1" value="Money Heist">
+                    <br>
+                    <input type="text" name="show2" value="New girl">
+                    <br>
+                    <input type="text" name="show3" value="You me her">
+                    <br>
+                    <input type="text" name="show4" value="Atypical">
+                    <br>
+                    <input type="text" name="show5" value="Riverdale">
+                    <br>
+                    <input type="submit" value="Submit">
+                </form> 
             </div>
-        </div>
+
+            <?php
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                // collect value of input field
+            $show = htmlspecialchars($_REQUEST['show1']);
+            echo $show;
+            $show2 = htmlspecialchars($_REQUEST['show2']);
+            echo $show2;
+            $show3 = htmlspecialchars($_REQUEST['show3']);
+            echo $show3;
+            $show4 = htmlspecialchars($_REQUEST['show4']);
+            echo $show4;
+            $show5 = htmlspecialchars($_REQUEST['show5']);
+            echo $show5;
+            }
+            ?>
+    </div>
+        
         <!--GET PART-->
         <div class="row getter">
             <div class="col-6 country">
