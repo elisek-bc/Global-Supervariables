110165c3277c02e751d2964fa65f1d0435dd97a6
elisek-bc <elisekppns@gmail.com>
2019-05-13 10:29:08 +0200

    add html basic

---
+++ setup.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Global Variables PHP</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="stylesheet.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700|Oswald:400,700" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+</head>
+<body>
+    <div class="container">
+        <!--POST PART-->
+        <div class="row poster">
+            <div class="col-6 movies">
+                <h2>Top 5 Disney movies</h2>
+                <ol>
+                    <li>Up!</li>
+                    <li>The Lion King</li>
+                    <li>Wall-e</li>
+                    <li>Rapunzel</li>
+                    <li>Big Hero 6</li>
+                </ol>
+            </div>
+            <div class="col-6 shows">
+            <h2>Top 5 shows</h2>
+                <ol>
+                    <li>Money Heist</li>
+                    <li>New girl</li>
+                    <li>You me her</li>
+                    <li>Atypical</li>
+                    <li>Riverdale</li>
+                </ol>
+            </div>
+        </div>
+        <!--GET PART-->
+        <div class="row getter">
+            <div class="col-6 country">
+                <h2>Favourite country</h2>
+                <p>Italy</p>
+            </div>
+            <div class="col-6 worst">
+                <h2>Worst movie ever seen</h2>
+                <p>Into the wild</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
