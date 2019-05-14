51bc6a09051cb0819617c1d36b8484849a8dd1cb
elisek-bc <elisekppns@gmail.com>
2019-05-14 14:34:46 +0200

    non working bonus part tabs

--- src/result.php
+++ src/result.php
@@ -1,45 +1,90 @@
-
+<!--NAVIGATION-->
+<div class="row navigation m-auto d-inline-block text-center">
+    <ul class="nav nav-tabs">
+        <li><a href="#movies">movies</a></li>
+        <li><a href="#shows">shows</a></li>
+        <li><a href="#country">country</a></li>
+        <li><a href="#worst">worst movie</a></li>
+    </ul>
+</div>
+<!-------------------------------------------------------->
+<!--MAKING ARRAYS WITH PHP-->
 <?php
     require('header.php');
-    ?>
-    <div class='wrapper'>
+?>
+
+<div class='wrapper'>
+
     <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // collect value of input field
     // making arrays to store movies / shows
-        echo "<h2>My favourite Disney movies are: </h2>";
-        $movieArray = [];
-        $showArray = [];
+    
+            
+            $movieArray = [];
+            $showArray = [];
 
-        for($i=1; $i < 6; $i++) {
-            array_push($movieArray,htmlspecialchars($_POST["movie$i"]));
-        };
+            for($i=1; $i < 6; $i++) {
+                array_push($movieArray,htmlspecialchars($_POST["movie$i"]));
+            };
 
-        foreach($movieArray as $value){
-            echo "<p>$value</p>";       
+            
+            for($i=1; $i < 6; $i++) {
+                array_push($showArray,htmlspecialchars($_POST["show$i"]));
+            };
         }
+    ?>
+<!-------------------------------------------------------->
 
-        echo "<h2>My favourite shows are: </h2>";
-        for($i=1; $i < 6; $i++) {
-            array_push($showArray,htmlspecialchars($_POST["show$i"]));
-        };
+    <!--What needs to appear when you click the id with post-->
+    <!--MOVIES PART-->
+    <div id="movies">
+        <?php
+            echo "<h2>My favourite Disney movies are: </h2>";
+            foreach($movieArray as $value){
+                echo "<p>$value</p>";       
+            }
+        ?> 
+    </div>
+<!-------------------------------------------------------->
+    <!--SHOWS PART-->
+    <div id="shows">
+        <?php 
+            echo "<h2>My favourite shows are: </h2>";
+            foreach($showArray as $value){
+                echo "<p>$value</p>";       
+            }
+        ?>
+    </div>
+<!-------------------------------------------------------->
 
-        foreach($showArray as $value){
-            echo "<p>$value</p>";       
+    <!--What needs to appear when you click the id with get-->
+    <!-- COUNTRY PART-->
+    <?php 
+        if ($_SERVER["REQUEST_METHOD"] == "GET") {
+            $country = htmlspecialchars($_GET['country']);
+            $worst = htmlspecialchars($_GET['worst']);;
         }
+    ?>
 
-    }
+    <div id="country">
+    <h2>My favourite country is:</h2>
 
-    else if ($_SERVER["REQUEST_METHOD"] == "GET") {
-        echo "<h2>My favourite country is:</h2>";
-        $country = htmlspecialchars($_GET['country']);
+    <?php 
         echo "<p>$country</p>";
+    ?>
+    </div>
+<!-------------------------------------------------------->
 
-        echo "<h2>The worst movie I ever saw is: </h2>";
-        $movie = htmlspecialchars($_GET['worst']);
-        echo "<p>$movie</p>";
-    }  
-?>
-</div>
+    <!--WORST MOVIE PART-->
+    <div id="worst">  
+    <h2>The worst movie I ever saw is: </h2>
+    
+    <?php    
+        echo "<p>$worst</p>";  
+    ?>
+    </div>
+
+<!-------------------------------------------------------->
 </body>
 </html>
