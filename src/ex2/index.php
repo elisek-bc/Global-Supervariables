07a258e6bc5e6e248be5701690cf8b0b9122f85f
elisek-bc <elisekppns@gmail.com>
2019-05-16 11:39:17 +0200

    added minor layout changes such as shadows

--- src/ex2/index.php
+++ src/ex2/index.php
@@ -4,9 +4,9 @@
 
 <div class="wrapper">
     <div class="row">
-        <div class="col">
-            <h1>Pick an emoji</h1>
-            <h3>and click on submit to see if you won</h3>
+        <div class="col-lg-6 m-auto">
+            <h1>Pick your cheat meal</h1>
+            <h3>...and click on submit to see if it's your lucky day</h3>
             <form method="post" action="lottery.php">
                 <input type="radio" name="food" value="pizza" id="pizza" class="input-hidden">
                     <label for="pizza">🍕</label>
--- src/ex2/lottery.php
+++ src/ex2/lottery.php
@@ -1,3 +1,48 @@
 <?php
  require('header.php')
 ?>
+
+<div class="wrapper">
+    <div class="row">
+        <div class="col-lg-6 m-auto">
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $pickedFood = $_POST ['food'];
+    $foods = [
+        'pizza',
+        'burger',
+        'fries'
+    ];
+
+    $winner = array_rand($foods,1);
+
+    if ($pickedFood == $foods[$winner]){
+        switch ($pickedFood){
+            case 'pizza':
+                echo "<h1>You win a $pickedFood !</h1>";
+                echo '<p>🍕</p>';
+                break;
+            case 'burger':
+                echo "<h1>You win a $pickedFood !</h1>";
+                echo '<p>🍔</p>';
+                break;
+            case 'fries':
+                echo "<h1>You win a bowl of $pickedFood !</h1>";
+                echo '<p>🍟</p>';
+                break;
+            default:
+                echo "You haven't chosen a cheat meal";
+                return;
+        };
+    } else {
+        echo "<h1>Hmmm...no $pickedFood for you today...</h1>";
+    };
+};
+?>
+            <a href="index.php" class="btn btn-warning">Try again?</a>
+        </div>
+    </div>
+</div>
+
+    
