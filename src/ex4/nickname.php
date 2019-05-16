333cbdd26f916aee1c4fae1c7cfd5234e5ba3681
elisek-bc <elisekppns@gmail.com>
2019-05-16 17:02:12 +0200

    working form without manipulations

--- src/ex4/nickname.php
+++ src/ex4/nickname.php
@@ -14,13 +14,37 @@
         <div class="row m-auto">
             <div class="col m-auto text-center">
                 <h1>Cool nickname generator</h1>
-                <form method="post" action="test.php">
+                <form method="post">
                     <input type='text' name="nickname" placeholder="Type your name...">
-                    <input type='submit' name="submit" value="Generate">
+                    <input type="submit" name="submit" value="Generate" id="button">
                 </form>
+                <div id="results">
+                    <!--PHP STARTS HERE-->
+                    <?php
+                        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                            $nickname = htmlspecialchars($_POST['nickname']);
+                            echo $nickname;
+                        };
+                    ?>
+                    <!--PHP ENDS HERE-->
+                </div>
             </div>
         </div>
     </div>
+
+<script>
+    var results = document.getElementById('results');
+    var button = document.getElementById('button');
+    var row = document.querySelector('.row');
+    
+    button.addEventListener('click',showResults);
+    function showResults(e){
+        e.preventDefault(); // prevents reload: submit automatically reloads page
+        results.style.display = 'block';
+        row.style.top = '30%';
+    }
+</script>
+
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
